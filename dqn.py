16ed5bbac03403c8244c5659ea5cf27cf2573de9
uwangGood <thdwotkd123@gmail.com>
2023-08-13 14:27:12 +0900

    asd

--- dqn.py
+++ dqn.py
@@ -1,13 +1,124 @@
-import gym
-env = gym.make("LunarLander-v2", render_mode="human")
-env.action_space.seed(42)
+import gymnasium as gym
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
 
-observation, info = env.reset(seed=42)
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 
-for _ in range(120):
-    observation, reward, terminated, truncated, info = env.step(env.action_space.sample())
+env = gym.make("CartPole-v1")
 
-    if terminated or truncated:
-        observation, info = env.reset()
+# GPU를 사용할 경우
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-env.close()
+"""
+Transition - 우리 환경에서 단일 전환을 나타내도록 명명된 튜플. 그것은 화면의 차이인 state로 (state, action) 쌍을 (next_state, reward) 결과로 매핑합니다.
+"""
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+"""
+ReplayMemory - 최근 관찰된 전이를 보관 유지하는 제한된 크기의 순환 버퍼. 또한 학습을 위한 전환의 무작위 배치를 선택하기위한 .sample () 메소드를 구현합니다.
+"""
+
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """transition 저장"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+class DQN(nn.Module):
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    # 최적화 중에 다음 행동을 결정하기 위해서 하나의 요소 또는 배치를 이용해 호출됩니다.
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+
+
+# BATCH_SIZE는 리플레이 버퍼에서 샘플링된 트랜지션의 수입니다.
+# GAMMA는 이전 섹션에서 언급한 할인 계수입니다.
+# EPS_START는 엡실론의 시작 값입니다.
+# EPS_END는 앱실론의 최종 값입니다.
+# EPS_DECAY는 앱실론의 지수 감쇠(exponential decay) 속도 제어하며, 높을수록 감쇠 속도가 느립니다.
+# TAU는 목표 네트워크의 업데이트 속도입니다.
+# LR은 ''AdamW'' 옵티마이저의 학습률(learning rate)입니다.\
+
+BATCH_SIZE = 128
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 1000
+TAU = 0.005
+LR = 1e-4
+
+# gym 행동 공간에서 행동의 숫자를 얻습니다.
+n_actions = env.action_space.n
+# 상태 관측 횟수를 얻습니다.
+state, info = env.reset()
+n_observations = len(state)
+
+policy_net = DQN(n_observations, n_actions).to(device)
+target_net = DQN(n_observations, n_actions).to(device)
+target_net.load_state_dict(policy_net.state_dict())
+
+optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
+memory = ReplayMemory(100000)
+
+steps_done = 0
+
+
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            return policy_net(state).max(1)[1].view(1, 1)
+    else:
+        return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
+
+
+episode_durations = []
+
+
+def plot_durations(show_result=False):
+    plt.figure(1)
+    durations_t = torch.tensor(episode_durations, dtype=torch.float)
+    if show_result:
+        plt.title('Result')
+    else:
+        plt.clf()
+        plt.title('Tlaining...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(durations_t.numpy())
+
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+
+    plt.pause(0.001)
